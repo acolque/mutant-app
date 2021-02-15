4358b7e2d03b5d4f12419497f9cef3b685846c7d
ArnoldC <arnoldclq3@gmail.com>
2021-02-15 13:08:51 -0300

    Se agrega logica a BusinessMutant y se inyecta dependencias

--- business/mutantBusiness.go
+++ business/mutantBusiness.go
@@ -1,8 +1,18 @@
 package business
 
+import "github.com/mutant-app/services"
+
 type MutantBusiness struct {
+	myDetector services.IDetector
+}
+
+func NewMutantBusiness(detector services.IDetector) *MutantBusiness {
+	b := new(MutantBusiness)
+	b.myDetector = detector
+	return b
 }
 
 func (m MutantBusiness) IsMutant(dna []string) bool {
-	return true
+	result, _ := m.myDetector.IsMutant(dna)
+	return result
 }
--- test/mutantBusiness_test.go
+++ test/mutantBusiness_test.go
@@ -4,14 +4,26 @@ import (
 	"testing"
 
 	"github.com/mutant-app/business"
+	"github.com/mutant-app/services"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidateBusiness(t *testing.T) {
-	m := new(business.MutantBusiness)
+func TestValidateBusinessIsMutantSucess(t *testing.T) {
+	detector := new(services.DnaMutantDetector)
+	myBusiness := business.NewMutantBusiness(detector)
 	dna := []string{"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"}
 
-	result := m.IsMutant(dna)
+	result := myBusiness.IsMutant(dna)
 
-	assert.True(t, result, "fallo test TestValidateBusiness")
+	assert.True(t, result, "fallo test TestValidateBusinessIsMutantSucess")
+}
+
+func TestValidateBusinessIsMutantError(t *testing.T) {
+	detector := new(services.DnaMutantDetector)
+	myBusiness := business.NewMutantBusiness(detector)
+	dna := []string{"ATGCGA", "CAGTGC", "TTATGT", "AGAATG", "ACCCTA", "TCACTG"}
+
+	result := myBusiness.IsMutant(dna)
+
+	assert.False(t, result, "fallo test TestValidateBusinessIsMutantError")
 }
